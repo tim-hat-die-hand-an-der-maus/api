ca2a84cd391b633a66b0fd31915d7f907e3a1e6e
Björn Petersen <git@bjoernpetersen.net>
2021-11-17 23:08:22 +0100

    Go back to Java 11 because Quarkus doesn't like 17 yet

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ version = "0.1.0"
 tasks {
     withType<KotlinCompile> {
         kotlinOptions {
-            jvmTarget = "17"
+            jvmTarget = "11"
             javaParameters = true
             freeCompilerArgs = listOf(
                 "-Xjvm-default=all",
@@ -81,7 +81,7 @@ spotless {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion.set(JavaLanguageVersion.of(11))
     }
 }
 
