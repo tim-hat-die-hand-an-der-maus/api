c99e1cb2145465d38b62f6690bc0ce965c1d3798
Björn Petersen <git@bjoernpetersen.net>
2024-01-30 10:01:15 +0100

    chore(deps): Update Kotlin to 1.9.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.spotless)
     alias(libs.plugins.quarkus)
-    kotlin("jvm") version "1.9.20"
-    kotlin("kapt") version "1.9.20"
-    kotlin("plugin.jpa") version "1.9.20"
+    kotlin("jvm") version "1.9.22"
+    kotlin("kapt") version "1.9.22"
+    kotlin("plugin.jpa") version "1.9.22"
     alias(libs.plugins.versions)
-    kotlin("plugin.allopen") version "1.9.20"
+    kotlin("plugin.allopen") version "1.9.22"
 }
 
 group = "consulting.timhatdiehandandermaus"
