a73e091f4f4ffba34226b2e2d2bf5982694a313d
Björn Petersen <git@bjoernpetersen.net>
2025-01-11 13:36:00 +0100

    Replace kotlinx.cli package

--- src/main/kotlin/consulting/timhatdiehandandermaus/iface/cli/Main.kt
+++ src/main/kotlin/consulting/timhatdiehandandermaus/iface/cli/Main.kt
@@ -1,7 +1,10 @@
-@file:OptIn(ExperimentalCli::class)
-
 package consulting.timhatdiehandandermaus.iface.cli
 
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.Context
+import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.arguments.argument
 import consulting.timhatdiehandandermaus.application.usecase.GenerateToken
 import consulting.timhatdiehandandermaus.application.usecase.ShuffleQueue
 import consulting.timhatdiehandandermaus.application.usecase.UpdateAllMetadata
@@ -10,29 +13,23 @@ import io.quarkus.runtime.QuarkusApplication
 import io.quarkus.runtime.annotations.QuarkusMain
 import jakarta.enterprise.context.control.ActivateRequestContext
 import jakarta.inject.Inject
-import kotlinx.cli.ArgParser
-import kotlinx.cli.ArgType
-import kotlinx.cli.ExperimentalCli
-import kotlinx.cli.Subcommand
 import java.nio.file.Path
 import kotlin.io.path.writeText
 
 private class GenerateTokenCommand(
     private val runWithRequestContext: (() -> Unit) -> Unit,
     private val generateToken: GenerateToken,
-) : Subcommand(
-        "generate-token",
-        "Generate a JWT token",
-    ) {
+) : CliktCommand("generate-token") {
+    override fun help(context: Context) = "Generate a JWT token"
+
     val serviceName by argument(
-        ArgType.String,
         "service-name",
         "The name of the service to issue a token for. Will be the subject claim.",
     )
 
-    val outputFile: String by argument(ArgType.String, "output", "output path")
+    val outputFile: String by argument("output", "output path")
 
-    override fun execute() {
+    override fun run() {
         runWithRequestContext {
             val path = Path.of(outputFile)
             if (!path.isAbsolute) {
@@ -44,12 +41,10 @@ private class GenerateTokenCommand(
     }
 }
 
-private class RunApiCommand :
-    Subcommand(
-        "run-api",
-        "Serve the API",
-    ) {
-    override fun execute() {
+private class RunApiCommand : CliktCommand("run-api") {
+    override fun help(context: Context) = "Serve the API"
+
+    override fun run() {
         Quarkus.waitForExit()
     }
 }
@@ -57,11 +52,10 @@ private class RunApiCommand :
 private class ShuffleQueueCommand(
     private val runWithRequestContext: (() -> Unit) -> Unit,
     private val shuffleQueue: ShuffleQueue,
-) : Subcommand(
-        "shuffle-queue",
-        "Shuffle the current queue",
-    ) {
-    override fun execute() {
+) : CliktCommand("shuffle-queue") {
+    override fun help(context: Context) = "Shuffle the current queue"
+
+    override fun run() {
         runWithRequestContext {
             shuffleQueue()
         }
@@ -71,15 +65,18 @@ private class ShuffleQueueCommand(
 private class UpdateMetadataCommand(
     private val runWithRequestContext: (() -> Unit) -> Unit,
     private val updateAllMetadata: UpdateAllMetadata,
-) : Subcommand(
-        "update-metadata",
-        "Updates metadata of all movies in DB",
-    ) {
-    override fun execute() {
+) : CliktCommand("update-metadata") {
+    override fun help(context: Context) = "Updates metadata of all movies in DB"
+
+    override fun run() {
         runWithRequestContext {
             updateAllMetadata()
         }
     }
+}
+
+private class MainCommand : CliktCommand("main") {
+    override fun run() = Unit
 }
 
 @QuarkusMain
@@ -90,18 +87,17 @@ class Main
         private val shuffleQueue: ShuffleQueue,
         private val updateAllMetadata: UpdateAllMetadata,
     ) : QuarkusApplication {
-        @OptIn(ExperimentalCli::class)
         override fun run(args: Array<String>): Int {
-            val parser = ArgParser("application")
+            val main =
+                MainCommand()
+                    .subcommands(
+                        GenerateTokenCommand(this::withRequestContext, generateToken),
+                        RunApiCommand(),
+                        ShuffleQueueCommand(this::withRequestContext, shuffleQueue),
+                        UpdateMetadataCommand(this::withRequestContext, updateAllMetadata),
+                    )
 
-            parser.subcommands(
-                GenerateTokenCommand(this::withRequestContext, generateToken),
-                RunApiCommand(),
-                ShuffleQueueCommand(this::withRequestContext, shuffleQueue),
-                UpdateMetadataCommand(this::withRequestContext, updateAllMetadata),
-            )
-
-            parser.parse(args)
+            main.main(args)
 
             return 0
         }
