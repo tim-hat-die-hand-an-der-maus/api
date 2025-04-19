f439c1b577e1a5ab6ff9af041101dcd12cea0ecc
Björn Petersen <git@bjoernpetersen.net>
2025-04-19 16:46:27 +0200

    Drop invalid movies

---
+++ src/main/resources/db/migration/V12__delete_invalid.sql
@@ -0,0 +1,6 @@
+delete
+from movie
+where id in ('ca1f139b-2b1b-4bb5-9904-680cabe1331a',
+             'b68f7532-5022-4e83-893d-e8a0cf08d92d',
+             'fb0d6a33-3891-4c48-90b5-562afcebe7a8'
+  );
