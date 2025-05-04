955ef62f2314786d879db8f0ac5fb2336836b137
Björn Petersen <git@bjoernpetersen.net>
2025-05-04 19:44:03 +0200

    Register MetadataSourceType for reflection

--- src/main/kotlin/consulting/timhatdiehandandermaus/application/model/MetadataSourceType.kt
+++ src/main/kotlin/consulting/timhatdiehandandermaus/application/model/MetadataSourceType.kt
@@ -1,5 +1,8 @@
 package consulting.timhatdiehandandermaus.application.model
 
+import io.quarkus.runtime.annotations.RegisterForReflection
+
+@RegisterForReflection
 enum class MetadataSourceType {
     IMDB,
     TMDB,
