49c4f7907cdc18a4458cd43a1e7da37a1fe15b3c
Björn Petersen <git@bjoernpetersen.net>
2025-05-07 10:32:49 +0200

    Fix reflection

--- src/main/kotlin/consulting/timhatdiehandandermaus/application/model/MovieMetadata.kt
+++ src/main/kotlin/consulting/timhatdiehandandermaus/application/model/MovieMetadata.kt
@@ -1,12 +1,15 @@
 package consulting.timhatdiehandandermaus.application.model
 
+import io.quarkus.runtime.annotations.RegisterForReflection
 import java.time.Instant
 
+@RegisterForReflection
 data class CoverMetadata(
     val url: String,
     val ratio: Double,
 )
 
+@RegisterForReflection
 data class MovieMetadata(
     /** IMDb/TMDB ID */
     val id: String,
